3bb3d28bcc34ba953846be8c431405ed8cd1960c
Todd S <info@tascs.net>
2025-01-27 10:03:08 -0700

    .

--- tests/test_update_api_data.py
+++
@@ -1,8 +0,0 @@
-def test_fetch_api(update_parcels) -> list[dict]:
-    assert len(update_parcels) == 15
-
-    # assert len(consumed_parcel_data) == 15
-
-
-if __name__ == "__main__":
-    print(test_fetch_api())
