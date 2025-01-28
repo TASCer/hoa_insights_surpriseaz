85424c2c4e77ac4b69a6d39d3d760d81c11cde67
Todd S <info@tascs.net>
2025-01-28 15:51:46 -0700

    WIP

--- tests/test_update_parcel_data.py
+++ tests/test_update_parcel_data.py
@@ -4,11 +4,20 @@ from sqlalchemy import text
 def test_update_parcel(connection, parse_parcel_seed_data, parse_parcel_update_data) -> list[dict]:
 
     # assert count == 15
-    assert len(parse_parcel_seed_data) == 15
-    assert len(parse_parcel_update_data) == 15
-    seed_names = [x for x in parse_parcel_seed_data] #if x.OWNER == "Todd Stevens"]
-    print(seed_names)
-
+    assert len(parse_parcel_seed_data) == 2
+    assert len(parse_parcel_update_data) == 2
+    seed_owners, seed_rentals = parse_parcel_seed_data #if x.OWNER == "Todd Stevens"]
+    update_owners, update_rentals = parse_parcel_update_data #if x.OWNER == "Todd Stevens"]
+    
+    owners = [x for x in seed_owners if x.APN == "509-11-455"]
+    new_owners = [x for x in update_owners if x.APN == "509-11-455"]
+    
+    assert owners[0].OWNER == "STEVENS TODD"
+    assert new_owners[0].OWNER == "BUYER NEW"
+    
+    
+    assert len(seed_rentals) == 4
+    assert len(update_rentals) == 3
 
 if __name__ == "__main__":
     updates = test_update_parcel()
