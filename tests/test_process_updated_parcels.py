e33a7b2af2798acd077918e6c1e3fc6ff9d12077
Todd S <info@tascs.net>
2025-02-04 09:02:32 -0700

    initial

---
+++ tests/test_process_updated_parcels.py
@@ -0,0 +1,7 @@
+from hoa_insights_surpriseaz import process_updated_parcels
+
+
+def test_proccess_updates():
+    updates = process_updated_parcels.get_new_insights()
+
+    assert len(updates) >= 0
