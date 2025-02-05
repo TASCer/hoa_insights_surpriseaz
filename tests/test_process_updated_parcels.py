c5c8f3ca82565422555cb7f72b16874bb87e7eae
Todd S <info@tascs.net>
2025-02-05 10:29:23 -0700

    rename

--- tests/test_process_updated_parcels.py
+++
@@ -1,7 +0,0 @@
-# from hoa_insights_surpriseaz import process_updated_parcels
-
-
-# def test_proccess_updates():
-#     updates = process_updated_parcels.get_new_insights()
-
-#     assert len(updates) >= 0
