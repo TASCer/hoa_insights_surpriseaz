e1d688f28ca4d614d4830214433c38a2dad81067
Todd S <info@tascs.net>
2025-02-05 10:29:39 -0700

    rename

--- tests/conftest.py
+++ tests/conftest.py
@@ -18,7 +18,7 @@ TEST_SEED_FILES_PATH: str = "./tests/input/json_seed_data/"
 TEST_UPDATE_FILES_PATH: str = "./tests/input/json_update_data/"
 TEST_MANAGEMENT_PDF_PATH: str = "./tests/input/HOA Contact List (PDF).pdf"
 TEST_MANAGEMENT_CSV_PATH: str = "./tests/output/csv/surpriseaz-hoa-management.csv"
-TEST_PARCELS_CONSTANTS: str = "./tests/database/test_parcel_constants.csv"
+TEST_PARCELS_CONSTANTS: str = "./tests/database/parcel_constants.csv"
 
 
 @pytest.fixture(scope="session")
@@ -80,13 +80,13 @@ def get_owner_update_data():
     return consumed_owner_update_data
 
 
-@pytest.fixture(scope="function")
-def parse_owner_update_data(get_update_parcel_data):
-    test_parsed_owners_update_data, test_parsed_rentals_update_data = parse(
-        get_update_parcel_data
-    )
+# @pytest.fixture(scope="function")
+# def parse_owner_update_data(get_update_parcel_data):
+#     test_parsed_owners_update_data, test_parsed_rentals_update_data = parse(
+#         get_update_parcel_data
+#     )
 
-    return test_parsed_owners_update_data, test_parsed_rentals_update_data
+#     return test_parsed_owners_update_data, test_parsed_rentals_update_data
 
 
 # @pytest.fixture(scope="function")
