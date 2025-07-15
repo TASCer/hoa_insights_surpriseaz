4e3585cceb144a65bb9c7c2e05e19a1b351ea33f
Todd S <info@tascs.net>
2025-07-15 09:50:04 -0700

    reworked

--- src/hoa_insights_surpriseaz/utils/rename_files.py
+++ src/hoa_insights_surpriseaz/utils/rename_files.py
@@ -7,7 +7,7 @@ logger: Logger = logging.getLogger(__name__)
 
 
 def rename(old: Path, new: Path) -> bool:
-    if old:
+    if old.exists():
         try:
             old.replace(new)
             logger.info(f"FILE: {old.name} renamed to: {new.name}")
@@ -19,6 +19,26 @@ def rename(old: Path, new: Path) -> bool:
 
             return False
 
+    else:
+        return False
+
 
 if __name__ == "__main__":
-    rename()
+    # ORIGINAL RENAME
+    rename(
+        old=Path(
+            "/home/todd/python_projects/hoa_insights_surpriseaz/tests/input/TEST-ORIGINAL-PDF.pdf"
+        ),
+        new=Path(
+            "/home/todd/python_projects/hoa_insights_surpriseaz/tests/input/TEST-RENAMED-PDF.pdf"
+        ),
+    )
+    # PUT BACK
+    # rename(
+    #     old=Path(
+    #         "/home/todd/python_projects/hoa_insights_surpriseaz/tests/input/TEST-RENAMED-PDF.pdf"
+    #     ),
+    #     new=Path(
+    #         "/home/todd/python_projects/hoa_insights_surpriseaz/tests/input/TEST-ORIGINAL-PDF.pdf"
+    #     ),
+    # )
