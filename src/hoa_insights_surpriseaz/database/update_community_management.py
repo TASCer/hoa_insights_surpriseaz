4d0905195c7274f1f8bb51a4efcaf8fefc7e7749
Todd S <info@tascs.net>
2025-07-15 11:05:25 -0700

    typing

--- src/hoa_insights_surpriseaz/database/update_community_management.py
+++ src/hoa_insights_surpriseaz/database/update_community_management.py
@@ -63,7 +63,7 @@ def update(file: str = None) -> None:
             db_item = DBCM(**item.model_dump())
 
             try:
-                insert_qry: TextClause = f"""UPDATE {LOCAL_DB_NAME}.{MANAGEMENT_TABLE} 
+                insert_qry: str = f"""UPDATE {LOCAL_DB_NAME}.{MANAGEMENT_TABLE} 
                 SET BOARD_SITUS='{db_item.BOARD_SITUS}', BOARD_CITY='{db_item.BOARD_CITY}', MANAGER='{db_item.MANAGER}', CONTACT_ADX='{db_item.CONTACT_ADX}', CONTACT_PH='{db_item.CONTACT_PH}'
                 WHERE ID = '{id}'
                     ;"""
@@ -94,7 +94,7 @@ def update(file: str = None) -> None:
             db_item = DBCM(**item.model_dump())
 
             try:
-                insert_qry: TextClause = f"""UPDATE {REMOTE_DB_NAME}.{MANAGEMENT_TABLE} 
+                insert_qry: str = f"""UPDATE {REMOTE_DB_NAME}.{MANAGEMENT_TABLE} 
                 SET BOARD_SITUS='{db_item.BOARD_SITUS}', BOARD_CITY='{db_item.BOARD_CITY}', MANAGER='{db_item.MANAGER}', CONTACT_ADX='{db_item.CONTACT_ADX}', CONTACT_PH='{db_item.CONTACT_PH}'
                 WHERE ID = '{id}'
                     ;"""
