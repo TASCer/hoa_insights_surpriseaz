e3f002fde273be2c778c8390d431194f546f63ad
Todd S <info@tascs.net>
2025-07-15 11:04:11 -0700

    renamed file

---
+++ src/hoa_insights_surpriseaz/utils/file_renamer.py
@@ -0,0 +1,23 @@
+import logging
+
+from logging import Logger
+from pathlib import Path
+
+logger: Logger = logging.getLogger(__name__)
+
+
+def rename(old: Path, new: Path) -> bool:
+    if old.exists():
+        try:
+            old.replace(new)
+            logger.info(f"FILE: {old.name} renamed to: {new.name}")
+
+            return True
+
+        except FileNotFoundError as ffe:
+            logger.error(ffe)
+
+            return False
+
+    else:
+        return False
