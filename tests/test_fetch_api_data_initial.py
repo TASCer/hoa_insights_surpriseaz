56f7c45a6b16888e7999425f618f0e5b0ed1b42c
Todd S <info@tascs.net>
2025-08-23 12:37:49 -0700

    cleaned

--- tests/test_fetch_api_data_initial.py
+++ tests/test_fetch_api_data_initial.py
@@ -3,11 +3,3 @@ def test_api_seed(get_owner_seed_data) -> list[dict]:
     assert len(get_owner_seed_data) == 15
 
 
-# def test_api_update(get_owner_update_data) -> list[dict]:
-#     assert type(get_owner_update_data) is list
-#     assert len(get_owner_update_data) == 5
-
-
-if __name__ == "__main__":
-    print(test_api_seed())
-    # print(test_api_update())
