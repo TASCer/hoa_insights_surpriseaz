17e177b5aa63df3edcc83daeab22b5d6ff00d16a
Todd S <info@tascs.net>
2025-08-25 14:01:18 -0700

    WORKING!!

--- tests/test_update_local_tables.py
+++ tests/test_update_local_tables.py
@@ -2,6 +2,17 @@ from hoa_insights_surpriseaz.database import update_local_tables
 from hoa_insights_surpriseaz.my_secrets import test_debian_uri, test_debian_dbname
 
 
+def test_update_local_tables_origianal(parse_original_parcel_data) -> list[dict]:
+    update_local_tables.owners(
+        parse_original_parcel_data[0], db_name=test_debian_dbname, db_uri=test_debian_uri
+    )
+    update_local_tables.rentals(
+        parse_original_parcel_data[1], db_name=test_debian_dbname, db_uri=test_debian_uri
+    )
+
+
+
+
 def test_update_local_tables(parse_new_parcel_data) -> list[dict]:
     update_local_tables.owners(
         parse_new_parcel_data[0], db_name=test_debian_dbname, db_uri=test_debian_uri
