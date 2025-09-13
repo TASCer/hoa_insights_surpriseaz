666dabfed41362c36251bbd68574ce0bd75a2df7
Todd S <info@tascs.net>
2025-09-13 13:19:56 -0700

    wip: Path and not getting results

--- tests/test_update_remote_tables.py
+++ tests/test_update_remote_tables.py
@@ -5,14 +5,14 @@ from hoa_insights_surpriseaz.my_secrets import (
     test_bluehost_dbname,
 )
 from pathlib import Path
-
-
-# def test_update_remote_tables() -> None:
-#     p: Path = Path.cwd() / "src" / "rental_insights" / "output" / "csv" / "financial"
-#     # assert p is type(Path())
-#     assert (
-#         update_remote_tables.all(
-#             file_path=p, local_db=test_debian_uri, remote_db=test_bluehost_uri
-#         )
-#         is None
-#     )
+# from hoa_insights_surpriseaz.main import 
+# TODO FINISH THIS
+def test_update_remote_tables() -> None:
+    p: Path = Path.cwd().parent / "hoa_insights_surpriseaz" / "src" / "hoa_insights_surpriseaz" / "output" / "csv" / "financial" 
+    # assert p is type(Path())
+    assert (
+        update_remote_tables.all(
+            file_path=p, local_db=test_debian_uri, remote_db=test_bluehost_uri
+        )
+        is None
+    )
