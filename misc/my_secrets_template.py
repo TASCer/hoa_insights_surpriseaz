49f95ab5854991ac35115620ca2596abd2ead911
Todd S <info@tascs.net>
2025-09-30 08:49:14 -0700

    format

--- misc/my_secrets_template.py
+++ misc/my_secrets_template.py
@@ -15,14 +15,14 @@ prod_bluehost_uri = f"{prod_debian_dbuser}:{prod_debian_dbpass}@{prod_debian_dbh
 # TESTS DATABASE ARE  THE SAME
 
 # API for mcassessor urls
-api_header_type = 'AUTHORIZATION'
-api_header_creds = 'your auth key here'
+api_header_type = "AUTHORIZATION"
+api_header_creds = "your auth key here"
 api_user_agent = None
 
 # EMAIL INFO
 postfix_user = "username"
 postfix_password = "userpass"
-postfix_mailhost = 'email host'
+postfix_mailhost = "email host"
 
 # URLs
 test_pdf_url = "URL to other website to download pdf file"
