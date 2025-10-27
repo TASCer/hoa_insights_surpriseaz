15796e0ae7bdb4657041521438149ebc306360b2
TASCer <me@example.com>
2025-10-27 08:06:28 -0700

    renamed

---
+++ src/hoa_insights_surpriseaz/database/check_remote_database.py
@@ -0,0 +1,32 @@
+import logging
+
+from hoa_insights_surpriseaz import my_secrets
+from logging import Logger
+from sqlalchemy import exc, Engine
+from sqlalchemy_utils import database_exists, create_database
+
+REMOTE_DB_HOSTNAME: str = f"{my_secrets.prod_remote_dbhost}"
+REMOTE_DB_NAME: str = f"{my_secrets.prod_remote_dbname}"
+REMOTE_DB_USER: str = f"{my_secrets.prod_remote_dbuser}"
+REMOTE_DB_PW: str = f"{my_secrets.prod_remote_dbpass}"
+REMOTE_DB_URI: str = f"{my_secrets.prod_remote_uri}"
+
+
+def schema(engine: Engine) -> bool:
+    """
+    Function creates remote database schema.
+
+    :param engine: database engine
+    :return: True if created
+    """
+    logger: Logger = logging.getLogger(__name__)
+    try:
+        if not database_exists(engine.url):
+            create_database(engine.url)
+
+    except (exc.SQLAlchemyError, exc.OperationalError) as e:
+        logger.critical(str(e))
+
+        return False
+
+    return True
