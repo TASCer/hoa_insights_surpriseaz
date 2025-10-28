24085c0a9d2beee82564df731a52c858985f04fb
TASCer <me@example.com>
2025-10-28 10:45:02 -0700

    moved database schema check

--- src/hoa_insights_surpriseaz/database/setup/create_local_database.py
+++ src/hoa_insights_surpriseaz/database/setup/create_local_database.py
@@ -1,9 +1,11 @@
 import logging
 
-from hoa_insights_surpriseaz.database import check_local_database, models_local
+from hoa_insights_surpriseaz.database import models_local
+from hoa_insights_surpriseaz.database.setup import create_local_triggers,create_local_views,create_local_procs
 
 from logging import Logger
-from sqlalchemy import Engine
+from sqlalchemy import Engine, exc
+from sqlalchemy_utils import database_exists, create_database
 
 logger: Logger = logging.getLogger(__name__)
 
@@ -15,11 +17,18 @@ def create(engine: Engine) -> bool:
     """
 
     logger.info(f"CREATNG DB ON: {engine.url.host}")
-    if check_local_database.schema(engine):
-        models_local.Base.metadata.create_all(engine)
-    triggers: bool = check_local_database.triggers(engine)
-    views: bool = check_local_database.views(engine)
-    stored_proc: bool = check_local_database.stored_procs(engine)
+    try:
+        if not database_exists(engine.url):
+            create_database(engine.url)
+
+    except (exc.SQLAlchemyError, exc.OperationalError) as e:
+        logger.critical(str(e))
+        return False
+
+    models_local.Base.metadata.create_all(engine)
+    triggers: bool = create_local_triggers.all(engine)
+    views: bool = create_local_views.all(engine)
+    stored_proc: bool = create_local_procs.all(engine)
     if all((triggers, views, stored_proc)):
         logger.info(f"CREATED DB ON: {engine.url.host}")
         return True
--- src/hoa_insights_surpriseaz/database/setup/create_remote_database.py
+++ src/hoa_insights_surpriseaz/database/setup/create_remote_database.py
@@ -1,21 +1,29 @@
 import logging
 
-from hoa_insights_surpriseaz.database import check_remote_database, models_remote
+from hoa_insights_surpriseaz.database import models_remote
 from logging import Logger
-from sqlalchemy import Engine
+from sqlalchemy import Engine, exc
+from sqlalchemy_utils import database_exists, create_database
 
 logger: Logger = logging.getLogger(__name__)
 
 
 def create(remote_engine: Engine) -> bool:
     """
-    Function creates remote database
+    Function creates the remote database
     """
     logger.info(f"CREATING DB ON: {remote_engine.url.host}")
 
-    if check_remote_database.schema(engine=remote_engine):
-        models_remote.Base.metadata.create_all(remote_engine)
-        logger.info(f"CREATED DB ON: {remote_engine.url.host}")
-        return True
-    else:
+    try:
+        if not database_exists(remote_engine.url):
+            create_database(remote_engine.url)
+
+    except (exc.SQLAlchemyError, exc.OperationalError) as e:
+        logger.critical(str(e))
+
         return False
+
+    models_remote.Base.metadata.create_all(remote_engine)
+    logger.info(f"CREATED DB ON: {remote_engine.url.host}")
+
+    return True
