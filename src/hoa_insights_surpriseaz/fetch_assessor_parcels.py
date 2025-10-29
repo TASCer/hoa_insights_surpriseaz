19b73623c381df6fd833f9b6b0cea5f0f4a40147
TASCer <me@example.com>
2025-10-29 11:11:46 -0700

    get all apns

--- src/hoa_insights_surpriseaz/fetch_assessor_parcels.py
+++ src/hoa_insights_surpriseaz/fetch_assessor_parcels.py
@@ -39,10 +39,7 @@ def get_parcel_apns() -> list[str]:
         engine: Engine = create_engine(f"mysql+pymysql://{LOCAL_DB_URI}")
         with engine.connect() as conn, conn.begin():
             q_apns: Sequence[Row[Tuple[str]]] = conn.execute(
-                select(models_local.Parcel.APN).where(
-                    models_local.Parcel.COMMUNITY == "GREENWAYPARC3"
-                )
-            ).all()
+                select(models_local.Parcel.APN)).all()
             APNs = [result[0] for result in q_apns]
 
         return APNs
