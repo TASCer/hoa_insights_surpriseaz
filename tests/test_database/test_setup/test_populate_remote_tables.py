137377f6c2c8308c5f103d02849df31df9e688da
TASCer <todd@tascs.net>
2026-01-28 13:26:46 -0700

    initial

---
+++ tests/test_database/test_setup/test_populate_remote_tables.py
@@ -0,0 +1,61 @@
+from hoa_insights_surpriseaz.database.setup import (
+    create_local_database,
+    create_remote_database,
+    populate_local_tables,
+    populate_remote_tables,
+)
+from pathlib import Path
+
+# PARCELS_SEED_FILE: Path = Path.cwd().parent.parent / "database" / "setup" / "seed_data" / "parcel_constants.csv"
+
+# TEST_INITIAL_PARCELS_PATH: Path = (
+#     Path.cwd() / "tests" / "input" / "original_parcel_json"
+# )
+# TEST_UPDATE_PARCELS_PATH: Path = Path.cwd() / "tests" / "input" / "new_parcel_json"
+# TEST_MANAGEMENT_CSV_PATH: Path = (
+#     Path.cwd() / "tests" / "output" / "csv" / "surpriseaz-hoa-management.csv"
+# )
+PARCELS_CONSTANTS: Path = (
+    Path.cwd()
+    / "src"
+    / "hoa_insights_surpriseaz"
+    / "database"
+    / "setup"
+    / "seed_data"
+    / "parcel_constants.csv"
+)
+print(PARCELS_CONSTANTS)
+
+# TODO temp (copy of populate local results) fix until I refactor initial setup
+# COMMUNITY_TOTALS = []
+
+
+# def test_create_local_dbms(test_create_local_engine) -> None:
+#     check_local: bool = create_local_database.create(test_create_local_engine)
+
+#     assert check_local
+
+
+# def test_create_remote_dbms(test_create_remote_engine) -> None:
+#     check_remote: bool = create_remote_database.create(test_create_remote_engine)
+
+#     assert check_remote
+
+
+# def test_populate_local_tables(test_create_local_session) -> None:
+#     parcel_totals = populate_local_tables.parcels(
+#         db=test_create_local_session, file=PARCELS_CONSTANTS
+# )
+
+#     assert parcel_totals
+
+#     community_totals = populate_local_tables.communities(
+#         db=test_create_local_session)
+
+#     # COMMUNITY_TOTALS = community_totals.copy()
+#     assert len(community_totals) > 0
+
+def test_populate_remote_tables(test_create_remote_session) -> None:
+    populate_remote_tables.communities(
+        remote_db=test_create_remote_session,
+    )
