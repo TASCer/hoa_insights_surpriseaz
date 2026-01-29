86321626abd5f2f53f53f3630f6ce7bf3366f08c
TASCer <todd@tascs.net>
2026-01-29 10:11:52 -0700

    fixed: typo

--- src/hoa_insights_surpriseaz/utils/file_copier.py
+++ src/hoa_insights_surpriseaz/utils/file_copier.py
@@ -15,18 +15,18 @@ WEB_SERVER_REPORT_PATH_WINDOWS = Path(
 )
 
 
-def linux_server(source, destination, source_check, destinantion_check) -> None:
+def linux_server(source, destination, source_check, destination_check) -> None:
     """
     Function copies files for Linux systems
     """
-    if source_check and destinantion_check:
+    if source_check and destination_check:
         try:
             os.system(f"cp {source} {destination}")
             logger.info(f"'{source.name}' sent to 'tascs.test' web server")
         except BaseException as e:
             logger.critical(f"'{source.name}' NOT sent to 'tascs.test' web server {e}")
 
-    if not destinantion_check and source_check:
+    if not destination_check and source_check:
         copy_to = Path("~")
         try:
             os.system(f"scp {source} todd@debian.tascs.test:{copy_to}")
