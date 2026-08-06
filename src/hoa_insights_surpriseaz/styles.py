cf324681492ee45841b37f91f01dd8bf890c71fe
todd <todd@tascs.net>
2026-08-06 08:57:32 -0700

    WIP: PDFKit to weasyprint

--- src/hoa_insights_surpriseaz/styles.py
+++ src/hoa_insights_surpriseaz/styles.py
@@ -1,31 +1,35 @@
 def parcel_updates() -> list[dict]:
     web_styles_changes = [
         dict(
+            selector="page",
+            props=[("size", "A3"), ("margin", "2cm")],
+        ),
+        dict(
             selector="th",
             props=[
                 ("color", "blue"),
                 ("border", "1px solid #eee"),
-                ("padding", "6px 7px"),
-                ("font-size", "13px"),
+                ("padding", "1px 2px"),
+                ("font-size", "8px"),
             ],
         ),
-        dict(
-            selector="td:nth-child(-n + 2)",
-            props=[("color", "purple"), ("font-size", "12px")],
-        ),
-        dict(selector="tr", props=[("color", "red"), ("font-size", "14px")]),
+        # dict(
+        #     selector="td:nth-child(-n + 2)",
+        #     props=[("color", "orange"), ("font-size", "21px")],
+        # ),
+        dict(selector="tr", props=[("color", "red"), ("font-size", "8px")]),
         dict(
             selector=" ",
             props=[
                 ("font-family", "Arial"),
                 ("text-align", "center"),
-                ("margin", "12px auto"),
-                ("border", "6px solid black"),
+                ("margin", "3px auto"),
+                ("border", "1px solid black"),
             ],
         ),
         dict(
             selector="caption",
-            props=[("caption-side", "top"), ("font-size", "16px"), ("color", "blue")],
+            props=[("caption-side", "top"), ("font-size", "11px"), ("color", "blue")],
         ),
     ]
 
@@ -40,7 +44,7 @@ def finance_updates() -> list[dict]:
                 ("color", "black"),
                 ("border", "1px solid #eee"),
                 ("padding", "6px 7px"),
-                ("background", "light-grey"),
+                ("background", "grey"),
                 ("font-size", "18px"),
             ],
         ),
